d0351b026e7b3ac1e2b075ea62f9c020ed0c57a4
borhan98 <borhanuddindns420@gmail.com>
2024-01-23 15:15:59 +0600

    User registration API created

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cewmzm0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,9 +24,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // all collections
+        const userCollection = client.db("HouseHunter").collection("users");
 
-        
+
+        /*-----------------------------------------------
+                    User Related APIs
+        ------------------------------------------------*/
+        app.post("/register", async (req, res) => {
+            let newUser = req.body;
+            const query = { email: newUser.email }
+            const user = await userCollection.findOne(query);
+            if (user?.email) {
+                return res.send({ message: "User already exists" })
+            }
+
+            // Secure password with bcrypt
+            const hashedPass = await bcrypt.hash(newUser.password, 10);
+            newUser.password = hashedPass;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
